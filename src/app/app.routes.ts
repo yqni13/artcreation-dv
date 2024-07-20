fe99220cd36883439d6ad5da1d42b81667ae7578
yqni13 <yqni13@protonmail.com>
2024-07-20 17:00:02 +0200

    feat: prepare routing; adapt folder structure
    Add navigation and footer as components in common folder.
    >> prepare routing to integrate in loop
    
    Add home component and var-directive.
    >> custom agalVar as additional structural directive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        data: {title: 'Home', showInNavbar: true, showInFooter: false }
+    },
+    // {
+    //     path: 'about',
+    //     component: demo,
+    //     title: 'About'
+    // },
+    // {
+    //     path: 'contact',
+    //     component: demo,
+    //     title: 'Contact'
+    // },
+    // {
+    //     path: 'faq',
+    //     component: demo,
+    //     title: 'FAQ'
+    // },
+    // {
+    //     path: 'gallery',
+    //     component: demo,
+    //     title: 'Gallery'
+    // },
+    // {
+    //     path: 'imprint',
+    //     component: demo,
+    //     title: 'Imprint'
+    // },
+    // {
+    //     path: 'privacy',
+    //     component: demo,
+    //     title: 'Privacy'
+    // },
+];
---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    standalone: true,
+    imports: []
+})
+export class HomeComponent {
+
+    constructor() {
+        //
+    }
+
+}
