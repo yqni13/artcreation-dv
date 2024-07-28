1053333076f0d86c5f71e600102cfcedc5479b1a
yqni13 <yqni13@protonmail.com>
2024-07-28 20:31:53 +0200

    feat: add footer comp + refactor define vars
    Add footer comp with basical structure and theme option styling.
    >>  left/right side credits/information and highlight styling
    
    Refactor define variables and connect footer to layout.
    >> additional defines and footer displays with fixed sizes currently

--- src/app/common/components/footer/footer.component.ts
+++ src/app/common/components/footer/footer.component.ts
@@ -3,6 +3,16 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
-    standalone: true
+    styleUrl: './footer.component.scss',
+    standalone: true,
+    imports: []
 })
-export class FooterComponent {}
+export class FooterComponent {
+
+    constructor() {
+        //
+    }
+
+    
+
+}
