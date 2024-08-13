f1078bfff182b2b2d9720044a873dd4a7cd2ff24
yqni13 <yqni13@protonmail.com>
2024-08-13 17:36:43 +0200

    feat: Add faq comp basic structure/style
    Add basic structure and styling including theme option based colors.
    >> faq are visualized as collapsables
    
    Add dummy data for FAQ.
    >> questions are somewhat logically verbalized, but unsorted
    >> answers are dummy data

--- src/app/modules/faq/faq.component.ts
+++ src/app/modules/faq/faq.component.ts
@@ -1,16 +1,32 @@
 import { Component } from "@angular/core";
+import { default as faqData } from "../../shared/data/faq-data.json";
+import { ErrorService } from "../../shared/services/error.service";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: 'app-faq',
     templateUrl: './faq.component.html',
     styleUrl: './faq.component.scss',
     standalone: true,
-    imports: []
+    imports: [
+        CommonModule
+    ]
 })
 export class FAQComponent {
 
-    constructor() {
-        //
+    protected data: Record<string, string>;
+    protected hasData: boolean;
+
+    constructor(private errorService: ErrorService) {
+        try {
+            this.data = faqData;
+            this.hasData = true;
+        } catch(err) {
+            this.errorService.handle(err);
+            this.data = {};
+            this.hasData = false;
+        }
     }
 
+    // TODO(yqni13): need custom sorting of object keys for FAQ?
 }
