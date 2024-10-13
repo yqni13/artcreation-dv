ba1f6d9dfb822fddb930a0bc20a0330e5fb77c11
yqni13 <yqni13@protonmail.com>
2024-10-13 13:58:14 +0200

    feat: adapt contact form
    Add snackbar information for special case in contact component.
    Add price to show up if subject 'art order' selected.
    Adapt type selection for different cases in contact component.
    >> conditional subject, price and type

--- src/app/modules/contact/contact.component.ts
+++ src/app/modules/contact/contact.component.ts
@@ -5,7 +5,7 @@ import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, 
 import { SubjectOptions } from "../../shared/enums/contact-subject.enum";
 import { MailService } from "../../shared/services/mail.service";
 import { Router, RouterModule } from "@angular/router";
-import { ArtworkOptions } from "../../shared/enums/artwork-option.enum";
+import { ArtworkOptions, ArtworkOptionsHandcraftOnly, ArtworkOptionsOrigORPrint, ArtworkOptionsPaintingOnly } from "../../shared/enums/artwork-option.enum";
 import { DataShareService } from "../../shared/services/data-share.service";
 import { filter, Subscription, tap } from "rxjs";
 import { TextInputComponent } from "../../common/components/form-components/text-input/text-input.component";
@@ -17,6 +17,9 @@ import  * as CustomValidators  from "../../common/helper/custom-validators";
 import { TranslateModule, TranslateService } from "@ngx-translate/core";
 import { NavigationService } from "../../shared/services/navigation.service";
 import { HttpObservationService } from "../../shared/services/http-observation.service";
+import { SnackbarMessageService } from "../../shared/services/snackbar.service";
+import { SnackbarOption } from "../../shared/enums/snackbar-option.enum";
+import { FloatPrecisionPipe } from "../../common/pipes/float-precision.pipe";
 
 @Component({
     selector: 'app-contact',
@@ -27,13 +30,15 @@ import { HttpObservationService } from "../../shared/services/http-observation.s
         CommonModule,
         CastAbstractToFormControlPipe,
         FormsModule,
+        FloatPrecisionPipe,
         ReactiveFormsModule,
         RouterModule,
         SelectInputComponent,
         TextInputComponent,
         TextareaInputComponent,
         TranslateModule
-    ]
+    ],
+    providers: [FloatPrecisionPipe]
 })
 export class ContactComponent implements OnInit, AfterViewInit, OnDestroy {
 
@@ -44,16 +49,24 @@ export class ContactComponent implements OnInit, AfterViewInit, OnDestroy {
     protected hasValidReferenceNr: boolean;
     protected hasReferenceFromParams: boolean;
     protected readonly: boolean;
+    protected isOrigORPrint: boolean;
+    protected hasPriceZero: boolean;
+    protected artworkPrice: number | null;
     protected selectedParams: Record<string, string>;
     protected subjectOptions = SubjectOptions;
-    protected artworkOptions = ArtworkOptions;
+    protected artworkOptionsAll = ArtworkOptions;
+    protected artworkOptionsOrigORPrint = ArtworkOptionsOrigORPrint;
+    protected artworkOptionsHandcraftOnly = ArtworkOptionsHandcraftOnly;
+    protected artworkOptionsPaintingOnly = ArtworkOptionsPaintingOnly;
 
     private subscriptionDataShare$: Subscription;
     private subscriptionHttpObservation$: Subscription;
 
     constructor(
         private httpObservationService: HttpObservationService,
+        private snackbarService: SnackbarMessageService,
         private refCheckService: ReferenceCheckService,
+        private floatPrecisionPipe: FloatPrecisionPipe,
         private dataShareService: DataShareService,
         private navigate: NavigationService,
         private translate: TranslateService,
@@ -66,19 +79,22 @@ export class ContactComponent implements OnInit, AfterViewInit, OnDestroy {
         this.hasValidReferenceNr = false;
         this.hasReferenceFromParams = false;
         this.readonly = true;
+        this.isOrigORPrint = false;
+        this.hasPriceZero = false;
+        this.artworkPrice = null;
         this.selectedParams = {}
         this.subscriptionDataShare$ = new Subscription(); 
-        this.subscriptionHttpObservation$ = new Subscription(); 
+        this.subscriptionHttpObservation$ = new Subscription();
     }
-    
+
     ngOnInit() {
         this.subscriptionDataShare$ = this.dataShareService.sharedData$.pipe(
             filter((x) => !!x), // double exclamation mark (!!) sets null/empty value to boolean (false) !!"" === false
             tap((data) => {
                 if(this.navigate.getPreviousUrl().includes(data['referenceNr'])) {
-                    this.checkParameters(data);
+                    this.checkParametersFromGallery(data);
                 } else {
-                    this.checkParameters(null);
+                    this.checkParametersFromGallery(null);
                 }
         })).subscribe();
 
@@ -90,20 +106,21 @@ export class ContactComponent implements OnInit, AfterViewInit, OnDestroy {
             filter((x) => x || !x),
             tap((isStatus200: boolean) => {
                 if(isStatus200) {
-                    this.checkParameters(null)
+                    this.checkParametersFromGallery(null)
                     this.initForm();
                 }
 
-                this.setButtonStatus(true);
+                this.setButtonUsage(true);
             })
         ).subscribe();
     }
-    
+
     private initForm() {
         this.contactForm = this.fb.group({
             subject: new FormControl('', Validators.required),
             referenceNr: new FormControl(''),
             type: new FormControl(''),
+            price: new FormControl(''),
             email: new FormControl('', [Validators.required, Validators.email]),
             honorifics: new FormControl('', Validators.required),
             title: new FormControl(''),
@@ -119,16 +136,24 @@ export class ContactComponent implements OnInit, AfterViewInit, OnDestroy {
             subject: this.hasSelectedParameters ? this.selectedParams['subject'] : '',
             referenceNr: this.hasSelectedParameters ? this.selectedParams['referenceNr'] : '',
             type: this.hasSelectedParameters ? this.selectedParams['type'] : '',
+            price: '',
             email: '',
             honorifics: '',
             title: '',
             firstName: '',
             lastName: '',
             message: ''
-        })
+        });
+        this.reassignValidators();
+        this.setPriceAndFormat(this.contactForm.get('referenceNr')?.value);
     }
 
-    private checkParameters(data: Record<string, string> | null) {
+    private reassignValidators() {
+        // by overwriting form with params from gallery, need to reassign necessary validators (case originalORprint)
+        this.contactForm.get('type')?.setValidators(Validators.required);
+    }
+
+    private checkParametersFromGallery(data: Record<string, string> | null) {
         if(data !== null && data !== undefined &&
             (data['referenceNr'] !== '' && data['type'] !== '' && data['subject'] !== '')) {
             this.hasSelectedParameters = true;
@@ -136,6 +161,8 @@ export class ContactComponent implements OnInit, AfterViewInit, OnDestroy {
             this.hasValidReferenceNr = true;
             this.readonly = true;
             this.selectedParams = data;
+            this.hasPriceZero = !!data['requestPrice'];
+            this.checkTypeParam(data['type'])            
         } else {
             this.hasSelectedParameters = false;
             this.hasReferenceFromParams = false;
@@ -145,18 +172,29 @@ export class ContactComponent implements OnInit, AfterViewInit, OnDestroy {
         }
     }
 
+    private checkTypeParam(type: string) {
+        if(type === ArtworkOptions.originalORprint) {
+            this.setOrigORPrintFlag(true);
+            this.selectedParams['type'] = '';
+        } else {
+            this.setOrigORPrintFlag(false);
+        }
+    }
+
     configRefNrByType() {
         const subject = this.contactForm.get('subject')?.value;        
         if(subject === SubjectOptions.artOrder || subject === SubjectOptions.specificInformation) {
             this.contactForm.get('referenceNr')?.markAsUntouched();
-            this.contactForm.get('referenceNr')?.setValidators([Validators.required, CustomValidators.invalidRefNrValidator(this.refCheckService)]);
+            this.contactForm.get('referenceNr')?.setValidators([Validators.required, CustomValidators.invalidRefNrValidator(this.refCheckService), CustomValidators.invalidRefNrLengthValidator()]);
             this.contactForm.get('referenceNr')?.markAsPristine();            
             this.contactForm.get('referenceNr')?.setValue('');
+            this.contactForm.get('price')?.setValue(null);
             this.readonly = false;
         } else {
             this.contactForm.get('referenceNr')?.clearValidators();
             this.contactForm.get('referenceNr')?.setValidators(CustomValidators.invalidRefNrValidator(this.refCheckService));
             this.contactForm.get('referenceNr')?.setValue('');
+            this.contactForm.get('price')?.setValue(null);
             this.configRefNrByChanges();
             this.hasReferenceFromParams = false;
             this.readonly = false;
@@ -174,32 +212,58 @@ export class ContactComponent implements OnInit, AfterViewInit, OnDestroy {
 
         if(this.refCheckService.checkReferenceValidity(refNr)) {
             this.hasValidReferenceNr = true;
-            this.contactForm.get('type')?.setValue(this.refCheckService.checkTypeByReference(refNr));
+            const artworkOption = this.refCheckService.checkTypeByReference(refNr);
+            this.setPriceAndFormat(refNr);
+
+            // handle artworkOption 'originalORprint' seperately
+            if(artworkOption === this.artworkOptionsAll.originalORprint) {
+                this.setOrigORPrintFlag(true);
+                this.contactForm.get('type')?.setValue('');
+            } else {
+                this.setOrigORPrintFlag(false);
+                this.contactForm.get('type')?.setValue(artworkOption);
+            }
+
+            // handle not-for-sale artwork seperately
+            if(this.contactForm.get('subject')?.value === SubjectOptions.artOrder && !this.refCheckService.checkSaleStatusByReference(refNr)) {
+                this.contactForm.get('subject')?.setValue(this.subjectOptions.specificInformation);
+                this.snackbarService.notify({
+                    title: 'Only specific request available.',
+                    text: 'Artwork is not for sale or has no price at the moment!',
+                    autoClose: true,
+                    type: SnackbarOption.info,
+                    displayTime: 7500
+                })
+            }
         }        
     }
 
-    configRefNrOnSubmit() {
-        // add this validator only on submit to avoid permanent validation in length check
-        this.contactForm.get('referenceNr')?.addValidators(CustomValidators.invalidRefNrLengthValidator());
-        const activateValidator = this.contactForm.get('referenceNr')?.value;
-        this.contactForm.get('referenceNr')?.setValue('');
-        this.contactForm.get('referenceNr')?.setValue(activateValidator);
+    configTypeByChanges(event: any) {
+        this.contactForm.get('type')?.setValue(event.target?.value);
+    }
+
+    private setOrigORPrintFlag(flag: boolean) {
+        this.isOrigORPrint = flag;
+    }
+
+    private setPriceAndFormat(reference: string) {
+        const price = this.floatPrecisionPipe.transform(this.refCheckService.checkPriceByReference(reference), 2)
+        this.contactForm.get('price')?.setValue(String('€ ' + price));
     }
 
     onSubmit() {
-        this.configRefNrOnSubmit();
         this.contactForm.markAllAsTouched();
 
         if(this.contactForm.invalid) {
             return;
         }
 
-        this.setButtonStatus(false);
+        this.setButtonUsage(false);
         this.mailService.setMailData(this.contactForm.getRawValue());
         this.mailService.sendMail().subscribe();
     }
 
-    setButtonStatus(enabled: boolean) {
+    setButtonUsage(enabled: boolean) {
         if(enabled) {
             this.submitButton.nativeElement.classList.remove('artdv-readonly');
         } else {
