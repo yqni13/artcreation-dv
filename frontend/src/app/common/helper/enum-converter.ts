b4d01719a1319ca66cd106efea79fb0ff6e6245a
yqni13 <yqni13@protonmail.com>
2025-03-20 19:05:00 +0100

    refactor: code review changes
    Refactor code regarding Code Review.
    >> ARTDV-16-purchase-status #76

--- frontend/src/app/common/helper/enum-converter.ts
+++ frontend/src/app/common/helper/enum-converter.ts
@@ -4,33 +4,21 @@ import { ArtTechnique } from "../../shared/enums/art-technique.enum";
 import { SaleStatus } from "../../shared/enums/sale-status.enum";
 
 export const isArtGenre = (value: any): ArtGenre | null => {
-    let enumVal = ''
-    if(value) {
-        enumVal = (value as string).toUpperCase();
-    }
+    const enumVal = value ? (value as string).toUpperCase() : '';
     return Object.values(ArtGenre).includes(value) ? ArtGenre[(enumVal as any) as keyof typeof ArtGenre] : null;
 };
 
 export const isArtMedium = (value: any): ArtMedium | null => {
-    let enumVal = ''
-    if(value) {
-        enumVal = (value as string).toUpperCase();
-    }
+    const enumVal = value ? (value as string).toUpperCase() : '';
     return Object.values(ArtMedium).includes(value) ? ArtMedium[(enumVal as any) as keyof typeof ArtMedium] : null;
 };
 
 export const isArtTechnique = (value: any): ArtTechnique | null => {
-    let enumVal = ''
-    if(value) {
-        enumVal = (value as string).toUpperCase();
-    }
+    const enumVal = value ? (value as string).toUpperCase() : '';
     return Object.values(ArtTechnique).includes(value) ? ArtTechnique[(enumVal as any) as keyof typeof ArtTechnique] : null;
 };
 
 export const isSaleStatus = (value: any): SaleStatus | null => {
-    let enumVal = ''
-    if(value) {
-        enumVal = (value as string).toUpperCase();
-    }
+    const enumVal = value ? (value as string).toUpperCase() : '';
     return Object.values(SaleStatus).includes(value) ? SaleStatus[(enumVal as any) as keyof typeof SaleStatus] : null;
 }
