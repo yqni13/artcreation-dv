a265611ba1c3967e25e20df14656f7299146739e
yqni13 <yqni13@protonmail.com>
2025-03-30 23:07:29 +0200

    feat: add print comp (no responsive styling)

--- frontend/src/app/modules/prints/prints.component.ts
+++ frontend/src/app/modules/prints/prints.component.ts
@@ -1,19 +1,76 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { TranslateModule, TranslateService } from "@ngx-translate/core";
+import { TranslateModule } from "@ngx-translate/core";
+import * as content from "../../../../public/assets/i18n/en.json";
+import { CommonModule } from "@angular/common";
+import { Subscription, tap } from "rxjs";
+import { ThemeObservationService } from "../../shared/services/theme-observation.service";
+import { ThemeOption } from "../../shared/enums/theme-option.enum";
+import { SubjectOptions } from "../../shared/enums/contact-subject.enum";
+import { BaseRoute } from "../../api/routes/base.route.enum";
+import { ImgFullscaleComponent } from "../../common/components/img-fullscale/img-fullscale.component";
 
 @Component({
     selector: 'app-prints',
     templateUrl: './prints.component.html',
     styleUrl: './prints.component.scss',
     imports: [
+        CommonModule,
+        ImgFullscaleComponent,
         RouterModule,
         TranslateModule
     ]
 })
-export class PrintsComponent {
+export class PrintsComponent implements OnInit, OnDestroy {
 
-    constructor(private translate: TranslateService) {
-        //
+    protected paperPrintsListLength: number;
+    protected baseRoute = BaseRoute;
+    protected isFullscale: boolean;
+    protected blockPriceInfo: any;
+    protected activeFullscaleImg: string;
+
+    private subscriptionThemeObservation$: Subscription;
+
+    constructor(
+        private readonly themeObserve: ThemeObservationService
+    ) {
+        this.paperPrintsListLength = Object.keys(content['modules']['prints']['paper']['list']).length;
+        this.isFullscale = false;
+        this.activeFullscaleImg = '';
+
+        this.subscriptionThemeObservation$ = new Subscription();
+    }
+
+    ngOnInit() {
+        this.subscriptionThemeObservation$ = this.themeObserve.themeOption$.pipe(
+            tap((theme: ThemeOption) => {
+                switch(theme) {
+                    case(ThemeOption.lightMode): {
+                        this.blockPriceInfo = {
+                            img: '/assets/prints/block3_lightmode.svg'
+                        }
+                        break;
+                    }
+                    case(ThemeOption.darkMode):
+                    default: {
+                        this.blockPriceInfo = {
+                            img: '/assets/prints/block3_darkmode.svg'
+                        }
+                    }
+                }
+            })
+        ).subscribe();
+    }
+
+    setActiveFullscaleImg(path: string) {
+        this.activeFullscaleImg = path;
+    }
+
+    navigateFullscale(flag: boolean) {
+        this.isFullscale = flag;
+    }
+
+    ngOnDestroy() {
+        this.subscriptionThemeObservation$.unsubscribe();
     }
 }
