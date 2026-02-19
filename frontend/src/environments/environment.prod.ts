07179f3c9aa28ab4acdc919a47220f21d57108f4
yqni13 <yqni13@protonmail.com>
2026-02-19 21:22:58 +0100

    feat: translations, route + env (api-url)

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment: Environment = {
     BUILD_MODE: '',
     STORAGE_URL: '',
     API_BASE_URL: '',
+    API_SUPPORT_URL: '',
     IV_POSITION: 0,
     AES_PASSPHRASE: '',
     PUBLIC_KEY: ''
