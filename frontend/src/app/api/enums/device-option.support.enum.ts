0ac3837295021cd2551d12ed0701468b83883cbc
yqni13 <yqni13@protonmail.com>
2026-02-24 21:05:25 +0100

    feat: enums, property update textarea comp, style

---
+++ frontend/src/app/api/enums/device-option.support.enum.ts
@@ -0,0 +1,7 @@
+export type SupportDeviceOption = 'mobile' | 'desktop' | 'other';
+
+export const SupportDeviceOption = {
+    MOBILE: 'mobile' as SupportDeviceOption,
+    DESKTOP: 'desktop' as SupportDeviceOption,
+    OTHER: 'other' as SupportDeviceOption
+};
---
+++ frontend/src/app/api/enums/ticket-option.support.enum.ts
@@ -0,0 +1,7 @@
+export type SupportTicketOption = 'bug' | 'support' | 'feedback';
+
+export const SupportTicketOption = {
+    BUG: 'bug' as SupportTicketOption,
+    SUPPORT: 'support' as SupportTicketOption,
+    FEEDBACK: 'feedback' as SupportTicketOption
+}
